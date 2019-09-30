d12a121a911635c5e849876a177f717e1b01c0d9
jixinyuan <496509528@qq.com>
2019-09-30 21:24:33 +0800

    新增方法

--- git/src/main/java/jxy/git/Test1.java
+++ git/src/main/java/jxy/git/Test1.java
@@ -3,8 +3,12 @@ package jxy.git;
 public class Test1 {
     public void ss(){
         System.out.println("hh");
+        System.out.println(123);
     }
     public int aa(){
         return 2;
+    }
+    public boolean ssss(){
+        return true;
     }
 }
